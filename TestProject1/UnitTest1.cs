e1320410a14a5c049cd338c3e691fa21b5ed33ef
Abhinav Agnihotri <Aagnihotri7347@conestogac.on.ca>
2021-06-07 00:39:25 +0530

    2nd Commit for Addition+Subtraction test Cases(i.e. = 6 TC's).

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -32,7 +32,33 @@ namespace TestProject1
             Assert.AreEqual(9.00d, Calculator.result); //assert2
         }
 
-        
+        [Test]
+        public void Test1_Subtraction_Eight_Minus_Three_EqualsTo_Five()
+        {
+            Calculator.num1 = 8.00d;//arrange
+            Calculator.num2 = 3.00d;//arrange
+            Calculator.Subtraction(Calculator.num1, Calculator.num2); //act
+            Assert.AreEqual(5.0d, Calculator.result); //assert
+        }
+
+        [Test]
+        public void Test2_Subtraction_NineDecimalFiveZero_Minus_FourDecimalFiveZero_EqualsTo_Five()
+        {
+            Calculator.num1 = 9.50d;//arrange
+            Calculator.num2 = 4.50d;//arrange
+            Calculator.Subtraction(Calculator.num1, Calculator.num2); //act
+            Assert.AreEqual(5.00d, Calculator.result); //assert
+        }
+
+        [Test]
+        public void Test3_Subtraction_Ten_Minus_Eight_EqualsTo_Two()
+        {
+            Calculator.num1 = 10.00d;//arrange
+            Calculator.num2 = 8.00d;//arrange
+            Calculator.Subtraction(Calculator.num1, Calculator.num2); //act
+            Assert.AreEqual(2.00d, Calculator.result); //assert
+        }
+
 
     }
 }
