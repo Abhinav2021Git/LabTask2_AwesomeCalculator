536e7d5e87b2d4c34f53914576a68dbe667e61eb
Abhinav Agnihotri <Aagnihotri7347@conestogac.on.ca>
2021-06-07 00:21:46 +0530

    Add project files.

---
+++ LabTask2/Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabTask2
+{
+    public class Calculator
+    {
+        public static double result;
+        public static double num1, num2;
+
+        public static void Addition(double num1, double num2)
+        {
+            try
+            {
+                result = Math.Round(num1 + num2, 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine("The Sum of {0} and {1} post rounding off 2-decimal digits(if exist) is = {2}", num1, num2, result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Subtraction(double num1, double num2)
+        {
+            try
+            {
+                result = Math.Round((num1 - num2), 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine("After Subtracting {1} from {0}, the result after rounding off 2-decimal digits(if exist) is : {2}", num1, num2, result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Multiplication(double num1, double num2)
+        {
+            try
+            {
+                result = Math.Round((num1 * num2), 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine("The Multiplication result of {0} and {1} post rounding off 2-decimal digits(if exist) is : {2}", num1, num2, result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Division(double num1, double num2)
+        {
+            try
+            {
+                if (num2 != 0)
+                {
+                    result = Math.Round((num1 / num2), 2, MidpointRounding.AwayFromZero);
+                    Console.WriteLine("The division result of {0} & {1} post rounding off 2-decimal digits(if exist) is : {2}", num1, num2, result);
+                }
+                else
+                    Console.WriteLine("Wrong input, please try again later");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ LabTask2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace LabTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("::::::Awesome Calculator:::::");
+                Console.WriteLine("-----Menu options are as follows-------");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+
+                Console.WriteLine("Hey User! Which operation you would like to perform?:");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter your 1st number here:");
+                Calculator.num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter your 2nd number here:");
+                Calculator.num2 = Convert.ToDouble(Console.ReadLine());
+
+
+                switch (choice)
+                {
+                    case 1:
+                        Calculator.Addition(Calculator.num1, Calculator.num2);
+                        break;
+
+                    case 2:
+                        Calculator.Subtraction(Calculator.num1, Calculator.num2);
+                        break;
+
+                    case 3:
+                        Calculator.Multiplication(Calculator.num1, Calculator.num2);
+                        break;
+
+                    case 4:
+                        Calculator.Division(Calculator.num1, Calculator.num2);
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry you have entered some wrong value(s) !!");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+        }
+}
