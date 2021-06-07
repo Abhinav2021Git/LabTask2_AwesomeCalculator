750d1017110ad2cfda5d5d5427dbb0e143372b2d
Abhinav Agnihotri <Aagnihotri7347@conestogac.on.ca>
2021-06-08 00:13:27 +0530

    Modified some piece of code in Calculator.cs such as added a divide by ZeroException + Main Menu.

--- LabTask2/Calculator.cs
+++ LabTask2/Calculator.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LabTask2
 {
@@ -13,7 +11,7 @@ namespace LabTask2
         {
             try
             {
-                result = Math.Round(n1 + n2, 2, MidpointRounding.AwayFromZero);
+                result = Math.Round((n1 + n2), 2, MidpointRounding.AwayFromZero);
                 Console.WriteLine("The Sum of {0} and {1} post rounding off 2-decimal digits(if exist) is = {2}", n1, n2, result);
             }
             catch (Exception e)
@@ -27,7 +25,7 @@ namespace LabTask2
             try
             {
                 result = Math.Round((n1 - n2), 2, MidpointRounding.AwayFromZero);
-                Console.WriteLine("After Subtracting {1} from {0}, the result after rounding off 2-decimal digits(if exist) is : {2}", n1, n2, result);
+                Console.WriteLine("After Subtracting {1} from {0} and the result after rounding off 2-decimal digits(if exist) is : {2}", n1, n2, result);
             }
             catch (Exception e)
             {
@@ -57,12 +55,22 @@ namespace LabTask2
                     result = Math.Round((n1 / n2), 2, MidpointRounding.AwayFromZero);
                     Console.WriteLine("The division result of {0} & {1} post rounding off 2-decimal digits(if exist) is : {2}", n1, n2, result);
                 }
+                else if (n2 == 0)
+                {
+                    result = (n1 / n2);
+                    if (result == double.PositiveInfinity)
+                    {
+                        Console.WriteLine("The result after dividing {0} by {1} is: {2} ", n1, n2, result);
+                        throw new DivideByZeroException("You can't divide any number by \"0\" !!! ");
+                    }
+                }
                 else
                     Console.WriteLine("Wrong input, please try again later");
             }
-            catch (Exception e)
+
+            catch (Exception ex)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(ex.Message);
             }
 
         }
--- LabTask2/Program.cs
+++ LabTask2/Program.cs
@@ -19,12 +19,14 @@ namespace LabTask2
                 Console.WriteLine("Hey User! Which operation you would like to perform?:");
                 int choice = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter your 1st number here:");
-                Calculator.num1 = Convert.ToDouble(Console.ReadLine());
+                if (choice == 1 || choice == 2 || choice == 3 || choice == 4)
+                {
+                    Console.WriteLine("Enter your 1st number here:");
+                    Calculator.num1 = Convert.ToDouble(Console.ReadLine());
 
-                Console.WriteLine("Enter your 2nd number here:");
-                Calculator.num2 = Convert.ToDouble(Console.ReadLine());
-
+                    Console.WriteLine("Enter your 2nd number here:");
+                    Calculator.num2 = Convert.ToDouble(Console.ReadLine());
+                }
 
                 switch (choice)
                 {
@@ -45,10 +47,11 @@ namespace LabTask2
                         break;
 
                     case 0:
+                        Console.WriteLine("User has entered \"0\" for Exit. See you next time");
                         break;
 
                     default:
-                        Console.WriteLine("Sorry you have entered some wrong value(s) !!");
+                        Console.WriteLine("Sorry you have provided some incorrect input(s) !!");
                         break;
                 }
             }
